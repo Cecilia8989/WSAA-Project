a2de296dd86d140752ffcb460899a479ea17740c
Cecilia Pastore <ceciliapastore89@gmail.com>
2024-05-12 18:19:37 +0100

    trying to add on pa

---
+++ configPA.py
@@ -0,0 +1,8 @@
+# Database configuration class for SQL database
+class DBConfigSQL:
+    HOST = "localhost"  # SQL database host
+    USER = "root"       # SQL database user
+    PASSWORD = ""  # SQL database password
+    
+class SecretKey:
+    KEY = ''
