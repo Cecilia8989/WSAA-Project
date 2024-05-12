62467a4dd4ce5d4f3f07bd14409a03491882b113
Cecilia Pastore <ceciliapastore89@gmail.com>
2024-05-12 21:39:40 +0100

    coma deleted

--- config_template.py
+++ config_template.py
@@ -1,7 +1,7 @@
 # Database configuration class for SQL database
 class DBConfigSQL:
-    HOST = "localhost"  # SQL database host
-    USER = "root"       # SQL database user
+    HOST = ""  # SQL database host
+    USER = ""       # SQL database user
     PASSWORD = ""  # SQL database password
     
 class SecretKey:
